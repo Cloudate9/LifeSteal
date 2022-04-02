81dbc1b667f1f5fd87b34c8b9e40cacf4d07f3cd
Cloudate9 <thealtfulthing@tutanota.com>
2022-04-02 11:32:27 -0700

    Fix a dupe glitch with /withdraw. Also change heart item to red dye.

--- src/main/kotlin/io/github/cloudate9/lifesteal/LifeSteal.kt
+++ src/main/kotlin/io/github/cloudate9/lifesteal/LifeSteal.kt
@@ -1,6 +1,7 @@
 package io.github.cloudate9.lifesteal
 
-import io.github.cloudate9.lifesteal.commands.Restore
+import dev.triumphteam.gui.builder.item.ItemBuilder
+import io.github.cloudate9.lifesteal.commands.Hearts
 import io.github.cloudate9.lifesteal.commands.Revive
 import io.github.cloudate9.lifesteal.commands.Withdraw
 import io.github.cloudate9.lifesteal.listeners.*
@@ -8,19 +9,34 @@ import net.kyori.adventure.text.minimessage.MiniMessage
 import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.NamespacedKey
-import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.ShapedRecipe
 import org.bukkit.plugin.java.JavaPlugin
 import java.net.InetSocketAddress
 
-class LifeSteal: JavaPlugin() {
+class LifeSteal : JavaPlugin() {
+
+    private val miniMessage = MiniMessage.miniMessage()
+
+    val heartFragmentModel =
+        ItemBuilder.from(Material.CARROT_ON_A_STICK)
+            .name(miniMessage.deserialize("<red>Heart Fragment</red>"))
+            .lore(miniMessage.deserialize("<light_purple>Use four of these to craft a heart!</light_purple>"))
+            .model(25001).build()
+
+    val heartItemModel =
+        ItemBuilder.from(Material.RED_DYE)
+            .name(miniMessage.deserialize("<red>Heart</red>"))
+            .lore(miniMessage.deserialize("<light_purple>Use one of these to gain a heart!</light_purple>"))
+            .model(25002).build()
+
+    val reviveItemModel =
+        ItemBuilder.from(Material.FIREWORK_STAR)
+            .name(miniMessage.deserialize("<red>Reviver</red>"))
+            .lore(miniMessage.deserialize("<light_purple>Use /revive with one of these to revive a player!</light_purple>"))
+            .model(25003).build()
 
     companion object {
         val ipList = mutableListOf<InetSocketAddress>()
-        val miniMessage = MiniMessage.miniMessage()
-        lateinit var heartFragmentModel: ItemStack
-        lateinit var heartItemModel: ItemStack
-        lateinit var reviveItemModel: ItemStack
     }
 
     override fun onEnable() {
@@ -38,97 +54,38 @@ class LifeSteal: JavaPlugin() {
         pluginManager.registerEvents(TabEdit(miniMessage), this)
         pluginManager.registerEvents(UnlockRecipe(), this)
 
-        getCommand("restore")?.setExecutor(Restore())
-        getCommand("revive")?.setExecutor(Revive(this))
-        getCommand("withdraw")?.setExecutor(Withdraw())
+        getCommand("hearts")?.setExecutor(Hearts(miniMessage))
+        getCommand("revive")?.setExecutor(Revive(this, miniMessage))
+        getCommand("withdraw")?.setExecutor(Withdraw(this, miniMessage))
     }
 
     private fun heartFragment() {
-
-        val fragment = ItemStack(Material.CARROT_ON_A_STICK)
-        val fragmentMeta = fragment.itemMeta
-
-        fragmentMeta.lore(
-            listOf(
-                miniMessage.deserialize("<light_purple>Use four of these to craft a heart!</light_purple>")
-            )
-        )
-
-        fragmentMeta.displayName(
-            miniMessage.deserialize("<red>Heart Fragment</red>")
-        )
-
-        fragmentMeta.setCustomModelData(25001)
-
-        fragment.itemMeta = fragmentMeta
-
-        val fragmentRecipe = ShapedRecipe(NamespacedKey.minecraft("heart_fragment"), fragment)
+        val fragmentRecipe = ShapedRecipe(NamespacedKey.minecraft("heart_fragment"), heartFragmentModel)
         fragmentRecipe.shape("ODO", "DTD", "ODO")
 
         fragmentRecipe.setIngredient('O', Material.OBSIDIAN)
         fragmentRecipe.setIngredient('D', Material.DIAMOND_BLOCK)
         fragmentRecipe.setIngredient('T', Material.TOTEM_OF_UNDYING)
         Bukkit.getServer().addRecipe(fragmentRecipe)
-
-        heartFragmentModel = fragment
     }
 
     private fun heartItem() {
-
-        val heart = ItemStack(Material.WARPED_FUNGUS_ON_A_STICK)
-        val heartMeta = heart.itemMeta
-
-        heartMeta.lore(
-            listOf(
-                miniMessage.deserialize("<light_purple>Use one of these to gain a heart!</light_purple>")
-            )
-        )
-
-        heartMeta.displayName(
-            miniMessage.deserialize("<red>Heart</red>")
-        )
-
-        heartMeta.setCustomModelData(25002)
-
-        heart.itemMeta = heartMeta
-
-        val heartRecipe = ShapedRecipe(NamespacedKey.minecraft("heart"), heart)
+        val heartRecipe = ShapedRecipe(NamespacedKey.minecraft("heart"), heartItemModel)
         heartRecipe.shape("FDF", "DED", "FDF")
 
         heartRecipe.setIngredient('F', heartFragmentModel)
         heartRecipe.setIngredient('D', Material.DIAMOND_BLOCK)
         heartRecipe.setIngredient('E', Material.ELYTRA)
         Bukkit.getServer().addRecipe(heartRecipe)
-
-        heartItemModel = heart
     }
 
     private fun reviveItem() {
-        val revive = ItemStack(Material.FIREWORK_STAR)
-        val reviveMeta = revive.itemMeta
-
-        reviveMeta.lore(
-            listOf(
-                miniMessage.deserialize("<light_purple>Use /revive with one of these to revive a player!</light_purple>")
-            )
-        )
-
-        reviveMeta.displayName(
-            miniMessage.deserialize("<red>Reviver</red>")
-        )
-
-        reviveMeta.setCustomModelData(25003)
-
-        revive.itemMeta = reviveMeta
-
-        val reviveRecipe = ShapedRecipe(NamespacedKey.minecraft("revive"), revive)
+        val reviveRecipe = ShapedRecipe(NamespacedKey.minecraft("revive"), reviveItemModel)
         reviveRecipe.shape("HNH", "NSN", "HNH")
 
         reviveRecipe.setIngredient('H', heartItemModel)
         reviveRecipe.setIngredient('N', Material.NETHERITE_INGOT)
         reviveRecipe.setIngredient('S', Material.NETHER_STAR)
         Bukkit.getServer().addRecipe(reviveRecipe)
-
-        reviveItemModel = revive
     }
 }
--- src/main/kotlin/io/github/cloudate9/lifesteal/commands/Withdraw.kt
+++ src/main/kotlin/io/github/cloudate9/lifesteal/commands/Withdraw.kt
@@ -1,8 +1,7 @@
 package io.github.cloudate9.lifesteal.commands
 
 import io.github.cloudate9.lifesteal.LifeSteal
-import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.format.TextColor
+import net.kyori.adventure.text.minimessage.MiniMessage
 import org.bukkit.Material
 import org.bukkit.attribute.Attribute
 import org.bukkit.command.Command
@@ -11,15 +10,13 @@ import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 
 
-
-class Withdraw: CommandExecutor {
+class Withdraw(private val lifeSteal: LifeSteal, private val miniMessage: MiniMessage) : CommandExecutor {
     override fun onCommand(sender: CommandSender, cmd: Command, label: String, args: Array<out String>): Boolean {
 
         if (sender !is Player) {
 
             sender.sendMessage(
-                Component.text("Only players can use /hsmp withdraw")
-                    .color(TextColor.color(Integer.parseInt("FF5555", 16)))
+                miniMessage.deserialize("<red>Only players can use /withdraw</red>")
             )
 
             return true
@@ -27,52 +24,46 @@ class Withdraw: CommandExecutor {
 
         if (sender.hasPermission("lifesteal.withdraw")) {
 
-            val heartsToWithdraw = if (args.isEmpty()) 1 else args[0].toInt()
+            val heartsToWithdraw = if (args.isEmpty()) 1 else if (args[0].toInt() < 1) 1 else args[0].toInt()
             if (sender.health <= heartsToWithdraw) {
 
                 // -2 cause ensure player always has 1 heart. heartsToWithdraw * 2 because we check health points, not hearts.
                 sender.sendMessage(
-                    Component.text("You don't have enough hearts!")
-                    .color(TextColor.color(Integer.parseInt("FF5555", 16)))
+                    miniMessage.deserialize("<red>You don't have enough hearts!</red>")
                 )
 
                 return true
             }
 
-            val oldHealth = sender.health
-            sender.getAttribute(Attribute.GENERIC_MAX_HEALTH)!!.baseValue -= 2.0
-            sender.health = oldHealth - 2
-
-            for (number in 1..heartsToWithdraw) {
-
-                if (hasAvaliableSlot(sender)) sender.inventory.addItem(LifeSteal.heartItemModel)
-                else sender.world.dropItemNaturally(sender.location, LifeSteal.heartItemModel)
-
+            var emptySlots = 0
+            for (itemStack in sender.inventory.contents!!) {
+                if (itemStack == null || itemStack.type == Material.AIR) {
+                    emptySlots++
+                }
             }
 
+            if (emptySlots < heartsToWithdraw) {
+                sender.sendMessage(
+                    miniMessage.deserialize("<red>You don't have enough space in your inventory!</red>")
+                )
+                return true
+            }
+
+            sender.getAttribute(Attribute.GENERIC_MAX_HEALTH)!!.baseValue -= heartsToWithdraw
+            for (number in 1..heartsToWithdraw) sender.inventory.addItem(lifeSteal.heartItemModel)
+
+
             sender.sendMessage(
-                Component.text("Your withdrew $heartsToWithdraw hearts!")
-                    .color(TextColor.color(Integer.parseInt("55FF55", 16)))
+                miniMessage.deserialize("<green>Your withdrew $heartsToWithdraw hearts!</green>")
             )
 
             return true
         }
         sender.sendMessage(
-            Component.text("Incorrect usage: command is /lifesteal withdraw (number of hearts)")
-                .color(TextColor.color(Integer.parseInt("FF5555", 16))))
+            miniMessage.deserialize("<green>Incorrect usage: command is /withdraw <number of hearts></green>")
+        )
 
         return true
 
     }
-
-    private fun hasAvaliableSlot(player: Player): Boolean {
-        for ((index, item) in player.inventory.contents!!.withIndex()) {
-            if (index > 35) return false
-            if (item == null || item.type == Material.AIR) {
-                return true
-            }
-        }
-        return false
-    }
-
 }
