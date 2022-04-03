4df020961d82946bf7ca88d796334d6b64bdcc43
Cloudate9 <thealtfulthing@tutanota.com>
2022-04-02 18:28:23 -0700

    Improve ban-kicker, and help prevent people from getting below 0.5 max health.

--- src/main/kotlin/io/github/cloudate9/lifesteal/listeners/CheckBan.kt
+++ src/main/kotlin/io/github/cloudate9/lifesteal/listeners/CheckBan.kt
@@ -2,16 +2,27 @@ package io.github.cloudate9.lifesteal.listeners
 
 import io.github.cloudate9.lifesteal.LifeSteal
 import net.kyori.adventure.text.Component
+import org.bukkit.attribute.Attribute
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent
 import org.bukkit.event.player.PlayerJoinEvent
 
-class CheckBan(private val lifeSteal: LifeSteal): Listener {
+class CheckBan(private val lifeSteal: LifeSteal) : Listener {
 
     @EventHandler
-    fun checkBan(e: PlayerJoinEvent) {
-        if (lifeSteal.config.getList("banned")!!.contains(e.player.uniqueId.toString())) {
-            e.player.kick(Component.text("You ran out of lives! Get someone to revive you to rejoin!"))
+    fun checkBan(e: AsyncPlayerPreLoginEvent) {
+        if (lifeSteal.config.getList("banned")!!.contains(e.uniqueId.toString())) {
+            e.disallow(
+                AsyncPlayerPreLoginEvent.Result.KICK_BANNED,
+                Component.text("You ran out of lives! Get someone to revive you to rejoin!")
+            )
         }
+    }
+
+    @EventHandler
+    fun ensureMinimumHealth(e: PlayerJoinEvent) {
+        if (e.player.getAttribute(Attribute.GENERIC_MAX_HEALTH)!!.baseValue < 1.0)
+            e.player.getAttribute(Attribute.GENERIC_MAX_HEALTH)!!.baseValue = 1.0
     }
 }
