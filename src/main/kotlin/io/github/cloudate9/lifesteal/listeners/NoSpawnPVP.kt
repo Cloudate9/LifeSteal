2db696d0d3eefae611213d2d64cf0744158c7f90
Cloudate9 <thealtfulthing@tutanota.com>
2022-04-02 23:16:56 -0700

    1.4.3: Fix no spawn pvp working on nether and end.

--- src/main/kotlin/io/github/cloudate9/lifesteal/listeners/NoSpawnPVP.kt
+++ src/main/kotlin/io/github/cloudate9/lifesteal/listeners/NoSpawnPVP.kt
@@ -1,6 +1,7 @@
 package io.github.cloudate9.lifesteal.listeners
 
 import net.kyori.adventure.text.minimessage.MiniMessage
+import org.bukkit.World
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -11,6 +12,7 @@ class NoSpawnPVP(private val miniMessage: MiniMessage) : Listener {
     @EventHandler
     fun pvp(e: EntityDamageByEntityEvent) {
         if (e.entity !is Player || e.damager !is Player) return
+        if (e.entity.world.environment != World.Environment.NORMAL || e.damager.world.environment != World.Environment.NORMAL) return
         if ((e.entity.location.x < 75.0 && e.entity.location.x > -75.0) ||
             (e.entity.location.z > 75.0 && e.entity.location.z < -75.0)
         ) {
